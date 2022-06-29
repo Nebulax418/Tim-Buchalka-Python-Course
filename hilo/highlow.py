0b4e21b668bf35d481d611b75ae71cbc718567bd
Archisman Nath <archismannath380@gmail.com>
2022-06-29 18:08:24 -0400

    hilo imporvement

---
+++ hilo/highlow.py
@@ -0,0 +1,30 @@
+"""Program outline:
+
+My guess is 500
+
+h == higher
+l == lower
+c == correct
+"""
+
+# low bound
+low = 1
+
+# high bound
+high = 1000
+
+
+while True:
+    print(f"Guessing from {low} to {high}")
+    guess = low + ((high - low) // 2)    
+    ex = input(f"My guess is {guess} - enter h for higher, l for lower, or c for correct: ").casefold()
+    
+    if ex == 'h':
+        # should be higher than the lower bound/midpoint/guess
+        low = guess + 1
+    elif ex == 'l':
+        # should be lower than the upper bound - and the midpoint/guess
+        high = guess - 1
+    elif ex == 'c':
+        print("Yes! I found it!")
+        break         
